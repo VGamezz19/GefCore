4dc1518e4a49eaa3b83232e5c8ebf223b167441c
Victor Gamez <victorgamez@MacBook-Pro-de-Victor.local>
2017-03-30 20:16:07 +0200

    modalInit

--- public/app/js/components/login/login-controller.js
+++ public/app/js/components/login/login-controller.js
@@ -1,3 +1,9 @@
 angular.module('login').controller('loginController',['$scope', function ($scope) {
     $scope.titulo = "login";
+    $scope.loginInit = function () {
+      console.log("hola");
+
+            $('.modal').modal('toggle')
+          
+        };
   }]);
