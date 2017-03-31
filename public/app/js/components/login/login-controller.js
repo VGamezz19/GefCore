f704f5728c9a9462e380dff76f7fb57238188964
Victor Gamez <victorgamez@MacBook-Pro-de-Victor.local>
2017-03-31 15:46:46 +0200

    commitLogin

--- public/app/js/components/login/login-controller.js
+++ public/app/js/components/login/login-controller.js
@@ -1,9 +1,12 @@
 angular.module('login').controller('loginController',['$scope', function ($scope) {
     $scope.titulo = "login";
     $scope.loginInit = function () {
+            $('.modal').modal('toggle');
+            $('#username').focus();
+        };
+    $scope.focus = function() {
+
       console.log("hola");
 
-            $('.modal').modal('toggle')
-          
-        };
+    };
   }]);
