4d3f73c5ff110bab6a7ab60da6ef762a4f178bfe
AlexjandroN7 <alejandro.urdiales.93@gmail.com>
2017-04-01 13:08:28 +0200

    reboot project

---
+++ config/express.js
@@ -0,0 +1,30 @@
+var express = require('express');
+    morgan = require('morgan');
+    compress = require('compression');
+    bodyParser = require('body-parser');
+    methodOverride = require('method-override');
+
+
+module.exports =function(){
+  var app = express();
+  if(process.env.NODE_ENV == 'development'){
+    app.use(morgan('dev'));
+  } else if(process.env.NODE_ENV === 'production'){
+    app.use(compress());
+  }
+
+
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+  app.set('views', 'app/views');
+  app.set('view engine', 'ejs');
+
+
+  require('../app/routes/indexServer-routes.js')(app);
+  app.use(express.static('./public'));
+  return app;
+};
--- server.js
+++ server.js
@@ -1,22 +1,15 @@
-var express = require('express');
-var path = require('path');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express = require('./config/express');
 var app = express();
-var port = process.env.PORT||4000;
-var mongoose = require('mongoose');
-var dbName = 'gefcore';
 
 
-app.use(express.static(path.join(__dirname, 'public/app')));
-app.get('*', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
-});
-app.listen(port, function () {
-  console.log('Example app listening on port 4000!');
+
+
+
+
+app.listen(4000, function(){
+  console.log('you are listening at port 4000');
 });
 
-mongoose.connect('mongodb://localhost/'+ dbName,function(error){
-  if(error){
-      throw error;
-   }else{
-      console.log('Conectado a MongoDB');
-}});
+module.exports = app;
