e0f4088ca9f8c9f5b476dc48f9fecfd96bc4882e
AlexjandroN7 <alejandro.urdiales.93@gmail.com>
2017-04-25 21:01:39 +0200

    production preparation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //le decimos a nuestro servidor el entorno en el que trabajamos:
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+const port = process.env.PORT || 4000;
 
 //cargamos las dependencias necesarias
 var mongoose = require('./config/mongoose'),
@@ -15,8 +16,8 @@ var passport = passport();
 
 //elegimos el puerto por el cual escucharemos
 
-app.listen(4000, function(){
-  console.log('you are listening at port 4000');
+app.listen(port, function(){
+  console.log(`you are listening at port ${port}`);
 });
 
 //exportamos la variable app
