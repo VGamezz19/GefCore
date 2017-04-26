89a20d9a6655f3a182a36466cc8c0b300352e1f1
AlexjandroN7 <alejandro.urdiales.93@gmail.com>
2017-04-26 18:13:27 +0200

    session secret

--- config/env/production.js
+++ config/env/production.js
@@ -1,4 +1,5 @@
 //configuración de producción
 module.exports = {
-    db: 'mongodb://heroku_8fsbwhs8:et2d4jfr33opkfsopjamv2jvbp@ds141358.mlab.com:41358/heroku_8fsbwhs8'
+    db: 'mongodb://heroku_8fsbwhs8:et2d4jfr33opkfsopjamv2jvbp@ds141358.mlab.com:41358/heroku_8fsbwhs8',
+    sessionSecret : 'developmentSessionSecret'
 };
