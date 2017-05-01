8485ffd644fb06448cabd1487693cfb8fa755be8
Victor Gamez <victorgamez@MacBook-Pro-de-Victor.local>
2017-05-01 20:18:00 +0200

    matesTemplate

--- public/js/components/matematicas/matematicas-controller.js
+++ public/js/components/matematicas/matematicas-controller.js
@@ -1,13 +1,7 @@
-
-
-
   angular.module('matematicas').controller('matematicasController',['$scope', function ($scope) {
-      this.hello = 'Hola. soy el componente testText';
-      this.suma = 2 +5;
-      $scope.test = "soy de la template test-text";
 
-      $scope.matesGamez= []
-
+      //========================================================================
+      //Peticion GET de toda la coleccion Matematicas
       var jsonObject = '{ "matematicas" : [' +
                       '{ "game":"Suma sin problemas" , "deficultad":"1", "Puntuacion" : "50" },' +
                       '{ "game":"Resta a ver si puedes" , "deficultad":"2", "Puntuacion" : "150" },' +
@@ -19,9 +13,24 @@
                       '{ "game":" asdsa dasdas das asd as" , "deficultad":"8", "Puntuacion" : "750" },' +
                       '{ "game":" ads dasds dasd asd" , "deficultad":"9", "Puntuacion" : "850" },' +
                       '{ "game":" asdsa dasd sds d asdas as" , "deficultad":"10", "Puntuacion" : "950" } ]}';
+      this.matesGames = JSON.parse(jsonObject);
+
+      //========================================================================
+      //Peticion GET de toda la informacion del Usuario Conectado
+
+      var jsonObjectUser = '{"usuario": [{"username": "vgamez","firstname": "victor",'  +
+      '"lastname": "gamez","email": "pictor.games@gmail.com","puntuacion": {"mates": 500,"ingles": 600},' +
+      '"matematicas": [{"juego": {"titulo": "suma se puedes 1","puntos": 100,"estado": 1}},{"juego": {' +
+				'"titulo": "suma se puedes 2","puntos": 100,"estado": 1}},{"juego": {"titulo": "suma se puedes 3",'+
+				'"puntos": 100,"estado": 0}}],"ingles": {"juego1": {"titulo": "hello 1","puntos": 100,"estado": 1},'+
+			'"juego2": {"titulo": "hello 2","puntos": 100,"estado": 1	},"juego3": {"titulo": "hello 3","puntos": 100,' +
+				'"estado": 0}}}]}';
+      var usuario = JSON.parse(jsonObjectUser);
 
 
-      $scope.matesGames = JSON.parse(jsonObject);
+      this.puntuacionMaxima = 600;
+
+
     }]);
 
 //module('testText').controller('testController',['$scope', function ($scope) {
--- public/js/components/profile/profile-controller.js
+++ public/js/components/profile/profile-controller.js
@@ -1,7 +1,5 @@
+angular.module('profile').controller('profileController',['$scope', function ($scope) {
 
-
-
-  angular.module('profile').controller('profileController',['$scope', function ($scope) {
       var jsonObject = '{"usuario": [{"username": "vgamez","firstname": "victor",'  +
       '"lastname": "gamez","email": "pictor.games@gmail.com","puntuacion": {"mates": 500,"ingles": 600},' +
       '"matematicas": [{"juego": {"titulo": "suma se puedes 1","puntos": 100,"estado": 1}},{"juego": {' +
@@ -10,13 +8,34 @@
 			'"juego2": {"titulo": "hello 2","puntos": 100,"estado": 1	},"juego3": {"titulo": "hello 3","puntos": 100,' +
 				'"estado": 0}}}]}';
 
-
       $scope.usuario = JSON.parse(jsonObject);
 
-      $scope.juegoMatesActual = function() {
-        console.log($scope.usuario.usuario[0].matematicas);
+      //========================FUNCIONES PROFILE===============================
 
+      //1--=====================================================================
+      //Funcion para sumar todos los puntos totales que lleva actualmente el
+      //usuario en Matematicas.
+
+      function puntosTotales ()  {
+        $scope.puntosTotalesMates = 0;
+
+        $scope.usuario.usuario[0].matematicas.forEach(function(juegosMatematicas) {
+
+            $scope.puntosTotalesMates = $scope.puntosTotalesMates + juegosMatematicas.juego.puntos;
+        });
+      }
+
+      //2--=====================================================================
+      //Funcion para saver el ultimo juego que esta cursando el usuario.
+      //Eso lo podemos saber gracias al estado del juego.
+      // Estado 0  --> El usuario ha desbloqueado el juego, pero aun no lo ha jugado
+      // Estado 1 --> El usuario ya ha jugado a ese juego
+      // Estado 2 --> El usuario no puede entrar al juego por falta de "Puntos"
+      // Estado 3 --> El juego ha sido pasado con 0 errores, y esta perfecto.
+
+      function juegoMatesActual() {
         var i = 0;
+
         $scope.usuario.usuario[0].matematicas.forEach(function(juegosMatematicas) {
             console.log(juegosMatematicas);
 
@@ -25,11 +44,21 @@
               return  $scope.ultimoNivel = juegosMatematicas.juego.titulo;
 
             } else {
-              return false;
+              return $scope.ultimoNivel = "Aun no has jugado ningun nivel";
             }
+
         });
 
-      };
+      }
+
+      //=============================Fin FUNCIONES PROFILE======================
+
+
+      //=======================Ejecucion de las funciones ======================
+
+      juegoMatesActual();
+
+      puntosTotales();
 
     }]);
 
