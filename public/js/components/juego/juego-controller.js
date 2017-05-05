25aa106a07401b3f5a155426ba3621d8423bff2d
Victor Gamez <victorgamez@MacBook-Pro-de-Victor.local>
2017-05-05 18:01:17 +0200

    single game

--- public/js/components/juego/juego-controller.js
+++ public/js/components/juego/juego-controller.js
@@ -9,7 +9,33 @@
       params: {'id': 'juego1'},
       headers : {'Accept' : 'application/json'}
     }).then(function successCallback(response) {
-          console.log(response.data); 
+          var jugandose = response.data;
+          $scope.juegos = [];
+
+          $scope.juegos.push(response.data.juego1);
+          $scope.juegos.push(response.data.juego2);
+          $scope.juegos.push(response.data.juego3);
+          $scope.juegos.push(response.data.juego4);
+          $scope.juegos.push(response.data.juego5);
+
+          var time = new Date();
+
+
+          //If jugador nunca ha entrado a este nivel.
+          var NewJuego = {identi: jugandose.identi ,titulo: jugandose.titulo,nivel: jugandose.nivel,  puntuacion: 0,  estado: 0, correctas: 0, incorrectas: 0, ultimoUso: time};
+
+          $http({
+            method: 'POST',
+            url: '/updateGame',
+            data: {'juego': NewJuego},
+          //  headers : {'Accept' : 'application/json'}
+        }).then(function successCallback(response) {
+            console.log(response.data);
+        }) ;
+
+          //Else
+          //===
+          //====
 
       }, function errorCallback(response) {
 
