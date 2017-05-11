2a90cac52e8ff0bd67c8fd3612fb8a9a05bf0162
Victor Gamez <victorgamez@MacBook-Pro-de-Victor.local>
2017-05-11 15:30:31 +0200

    profile

--- public/js/components/profile/profile-controller.js
+++ public/js/components/profile/profile-controller.js
@@ -1,14 +1,7 @@
 angular.module('profile').controller('profileController',['$scope', function ($scope) {
 
-      var jsonObject = '{"usuario": [{"username": "vgamez","firstname": "victor",'  +
-      '"lastname": "gamez","email": "pictor.games@gmail.com","puntuacion": {"mates": 500,"ingles": 600},' +
-      '"matematicas": [{"juego": {"titulo": "suma se puedes 1","puntos": 100,"estado": 1}},{"juego": {' +
-				'"titulo": "suma se puedes 2","puntos": 100,"estado": 1}},{"juego": {"titulo": "suma se puedes 3",'+
-				'"puntos": 100,"estado": 0}}],"ingles": {"juego1": {"titulo": "hello 1","puntos": 100,"estado": 1},'+
-			'"juego2": {"titulo": "hello 2","puntos": 100,"estado": 1	},"juego3": {"titulo": "hello 3","puntos": 100,' +
-				'"estado": 0}}}]}';
 
-      $scope.usuario = JSON.parse(jsonObject);
+      
 
       //========================FUNCIONES PROFILE===============================
 
@@ -28,10 +21,9 @@ angular.module('profile').controller('profileController',['$scope', function ($s
       //2--=====================================================================
       //Funcion para saver el ultimo juego que esta cursando el usuario.
       //Eso lo podemos saber gracias al estado del juego.
-      // Estado 0  --> El usuario ha desbloqueado el juego, pero aun no lo ha jugado
+      // Estado 0  --> El juego ya esta disponible
       // Estado 1 --> El usuario ya ha jugado a ese juego
-      // Estado 2 --> El usuario no puede entrar al juego por falta de "Puntos"
-      // Estado 3 --> El juego ha sido pasado con 0 errores, y esta perfecto.
+      // Estado 3 --> El juego ha sido jugado pero con muchos errors (recordar).
 
       function juegoMatesActual() {
         var i = 0;
@@ -54,7 +46,7 @@ angular.module('profile').controller('profileController',['$scope', function ($s
       //=============================Fin FUNCIONES PROFILE======================
 
 
-      
+
 
       //=======================Ejecucion de las funciones ======================
 
@@ -63,6 +55,3 @@ angular.module('profile').controller('profileController',['$scope', function ($s
       puntosTotales();
 
     }]);
-
-//module('testText').controller('testController',['$scope', function ($scope) {
-//para pasarle el $scope
