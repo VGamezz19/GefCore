90676b0288d8ddb051567eb2624f2beecb339e73
AlexjandroN7 <alejandro.urdiales.93@gmail.com>
2017-05-18 19:04:33 +0200

    password reset

--- app/models/usersServer-model.js
+++ app/models/usersServer-model.js
@@ -9,83 +9,93 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 
-    //Definimos nuestro nuevo Schema
+//Definimos nuestro nuevo Schema
 
 var userSchema = new Schema({
 
-    username:{type: String, unique: true,
-         required: 'Nombre de usuario obligatorio',
-         //removemos los espacios en blanco del username
-         trim: true
-},
-    firstName : String,
-    lastName : String,
-    email : {type: String, required: 'email necesario',
-    //validamos que sea de formato email
-    match: [/.+\@.+\..+/, 'Escribe un email correcto']},
-    password: {type: String, required: 'necesitas una contraseña', validate: [function(password){
-        return password && password.length > 6;
-    }, 'La contraseña debe ser de minimo 7 caracteres'
-]
-},
-    salt: {type: String},
-    provider: {type: String,
-    required: 'Provider obligatorio!'},
+    username: {
+        type: String, unique: true,
+        required: 'Nombre de usuario obligatorio',
+        //removemos los espacios en blanco del username
+        trim: true
+    },
+    firstName: String,
+    lastName: String,
+    email: {
+        type: String, required: 'email necesario',
+        //validamos que sea de formato email
+        match: [/.+\@.+\..+/, 'Escribe un email correcto']
+    },
+    password: {
+        type: String, required: 'necesitas una contraseña', validate: [function (password) {
+            return password && password.length > 6;
+        }, 'La contraseña debe ser de minimo 7 caracteres'
+        ]
+    },
+    salt: { type: String },
+    provider: {
+        type: String,
+        required: 'Provider obligatorio!'
+    },
     providerId: String,
     providerData: {},
-    createdAt: {type: Date, default: Date.now
+    createdAt: {
+        type: Date, default: Date.now
     },
     puntuacion: {
-      matematicas: Number,
-      ingles: Number
+        matematicas: Number,
+        ingles: Number
     },
     matematicas: [{
-      juego: {
-        identi: String,
-        titulo: String,
-        nivel: Number,
-        puntuacion: Number,
-        estado: Number,
-        correctas: Number,
-        incorrectas: Number,
-        ultimoUso: Date
-      }
+        juego: {
+            identi: String,
+            titulo: String,
+            nivel: Number,
+            puntuacion: Number,
+            estado: Number,
+            correctas: Number,
+            incorrectas: Number,
+            ultimoUso: Date
+        }
     }],
     ingles: [{
-      juego: {
-        identi: String,
-        titulo: String,
-        nivel: Number,
-        puntuacion: Number,
-        estado: Number,
-        correctas: Number,
-        incorrectas: Number,
-        ultimoUso: Date
-      }
-    }]
+        juego: {
+            identi: String,
+            titulo: String,
+            nivel: Number,
+            puntuacion: Number,
+            estado: Number,
+            correctas: Number,
+            incorrectas: Number,
+            ultimoUso: Date
+        }
+    }],
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
 });
 
 
-userSchema.virtual('fullname').get(function()
-{ return this.firstName + ' ' +
- this.lastName; this.firstName = splitName[0] ||
-  ''; this.lastName = splitName[1] || ''; });﻿
+userSchema.virtual('fullname').get(function () {
+    return this.firstName + ' ' +
+        this.lastName; this.firstName = splitName[0] ||
+            ''; this.lastName = splitName[1] || '';
+});
 
 //Usamos el middleware pre-save para el hash de la contraseña
-userSchema.pre('save', function(next){
-    if(this.password){
+userSchema.pre('save', function (next) {
+    if (this.password) {
         this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
         this.password = this.hashPassword(this.password);
     }
     next();
 });
 //creamos un metodo de instancia para el hashing
-userSchema.methods.hashPassword = function(password) {
+userSchema.methods.hashPassword = function (password) {
     return crypto.pbkdf2Sync(password, this.salt, 10000, 64, 'sha1').toString('base64');
 };
 //creamos el metodo para autenticar al usuario
-userSchema.methods.authenticate = function(password){
-        return this.password === this.hashPassword(password);
+userSchema.methods.authenticate = function (password) {
+    return this.password === this.hashPassword(password);
 };
 
 
