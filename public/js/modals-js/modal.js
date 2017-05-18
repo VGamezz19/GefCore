60817d07ad8b7d25ea2d547cb6c2d7ffe14723a7
Victor Gamez <victorgamez@MacBook-Pro-de-Victor.local>
2017-05-18 18:30:49 +0200

    50percentReadWebResponsive

--- public/js/modals-js/modal.js
+++ public/js/modals-js/modal.js
@@ -2,21 +2,21 @@
 //Cuando hace click en Entrar del menu
   $('.login').click(function(){
     $('.loginModal').modal('show');
-    setTimeout(focus, 300);
+  //  setTimeout(focus, 300);
   });
 //End Login Modal jquery
 
 //Start Register Modal jquery
 $('.register').click(function(){
   $('.registerModal').modal('show');
-  setTimeout(focusRegister, 300);
+  //setTimeout(focusRegister, 300);
 });
 
 //End Register Modal jquery
 
 $('.logeOut').click(function(){
   $('.checkModal').modal('show');
-  setTimeout(focusLogOut, 300);
+  //setTimeout(focusLogOut, 300);
 });
 
 //Functions Modal Focus
