3634853955f49ccea50d43ac24ebb2b9f2478c65
Victor Gamez <victorgamez@MacBook-Pro-de-Victor.local>
2017-05-18 20:57:36 +0200

    fullReadWeb responsive

--- public/js/modals-js/modal.js
+++ public/js/modals-js/modal.js
@@ -2,31 +2,37 @@
 //Cuando hace click en Entrar del menu
   $('.login').click(function(){
     $('.loginModal').modal('show');
-  //  setTimeout(focus, 300);
+    setTimeout(focus, 300);
   });
 //End Login Modal jquery
 
 //Start Register Modal jquery
 $('.register').click(function(){
   $('.registerModal').modal('show');
-  //setTimeout(focusRegister, 300);
+  setTimeout(focusRegister, 300);
 });
 
 //End Register Modal jquery
 
 $('.logeOut').click(function(){
   $('.checkModal').modal('show');
-  //setTimeout(focusLogOut, 300);
+  setTimeout(focusLogOut, 300);
 });
 
 //Functions Modal Focus
   function focus () {
-    $('#username').focus();
+    document.getElementById('loginModal').focus();
+    $('[tabindex=2]').focus();
+    $("#loginModal").focus();
   }
   function focusRegister () {
-    $('#firstName').focus();
+    document.getElementById('registerModal').focus();
+    $('[tabindex=4]').focus();
+    $("#registerModal").focus();
   }
 
   function focusLogOut () {
-    $('.close-loged').focus();
+    document.getElementById('check').focus();
+    $('[tabindex=6]').focus();
+    $("#check").focus();
   }
