6d642fe58e8a40402833186d631c28b34b3c9135
AUrdiales <alejandro.urdiales.93@gmail.com>
2017-06-01 15:18:26 +0200

    fixing sendgrid

--- app/controllers/usersServer-controller.js
+++ app/controllers/usersServer-controller.js
@@ -328,8 +328,8 @@ exports.forgot = (req, res) => {
         authentication:'plain',
         domain: 'heroku',
         auth: {
-          user:  'app69106898@heroku.com',
-          pass:'yxpgqprj3652'
+          user:  process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD
         }
       });
       var mailOptions = {
@@ -387,8 +387,8 @@ exports.resetConfirmation = (req, res, next) => {
         authentication:'plain',
         domain: 'heroku',
         auth: {
-          user:  'app69106898@heroku.com',
-          pass:'yxpgqprj3652'
+          user:  process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD
         }
       });
       var mailOptions = {
