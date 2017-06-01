25241b6a4c1e10f698702fa8cb76345cc972f84d
AUrdiales <alejandro.urdiales.93@gmail.com>
2017-06-01 15:19:13 +0200

    fixing sendgrid

--- app/controllers/usersServer-controller.js
+++ app/controllers/usersServer-controller.js
@@ -333,7 +333,7 @@ exports.forgot = (req, res) => {
         }
       });
       var mailOptions = {
-        from: 'gefcoreapp@gmail.com',
+        from: 'alexjandron7@gmail.com',
         to: req.body.email,
         subject: 'Recuperar contraseña',
         text: `http://${req.headers.host}/reset/${token}`
@@ -392,7 +392,7 @@ exports.resetConfirmation = (req, res, next) => {
         }
       });
       var mailOptions = {
-        from: 'gefcoreapp@gmail.com',
+        from: 'alexjandron7@gmail.com',
         to: user.email,
         subject: 'contraseña cambiada',
         text: `Confirmamos que su contraseña ha sido cambiada correctamente.`
