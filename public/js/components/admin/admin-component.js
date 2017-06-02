07485b42e9141390f200360a083de43733505d34
AUrdiales <alejandro.urdiales.93@gmail.com>
2017-06-02 16:48:50 +0200

    admi component

---
+++ public/js/components/admin/admin-component.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+angular.module('adminModule').
+  component('adminComponent', {
+    templateUrl: '/js/components/admin/admin-template.html',
+    controller: 'adminController'
+
+  })
